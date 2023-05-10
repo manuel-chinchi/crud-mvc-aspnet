7e2a755e3ba722d114dea217dcb1366ec4ec73e8
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-05-10 01:26:27 -0300

    add CRUD actions for Article model - add views create, edit - update controller

---
+++ crud/Controllers/ArticleController.cs
@@ -0,0 +1,88 @@
+﻿using crud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace crud.Controllers
+{
+    public class ArticleController : Controller
+    {
+        static Article article = new Article() { Id = 1, Name = "zapatilla", Description = "nike, talle 42" };
+
+        // GET: Article/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Article/Create
+        public ActionResult Create()
+        {
+            ViewBag.Message = "Ingrese los datos del artículo";
+            return View();
+        }
+
+        // POST: Article/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Article/Edit/5
+        public ActionResult Edit(int id = 1)
+        {
+            ViewBag.Description = "Datos del artículo";
+            return View(article);
+        }
+
+        // POST: Article/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Article/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Article/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ crud/Models/Article.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace crud.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        [DisplayName("Nombre")]
+        public string Name { get; set; }
+        [DisplayName("Descripción")]
+        public string Description { get; set; }
+        [DisplayName("Cantidad")]
+        public int Quantity { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+
+        public Article() { DateCreated = DateTime.UtcNow; DateUpdated = null; }
+    }
+}
