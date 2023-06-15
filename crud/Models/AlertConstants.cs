448423861bc532c210f51e3ce52cfc16f52594bc
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-06-14 21:31:35 -0300

    add class for constants

---
+++ crud/Models/AlertConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crud.Models
+{
+    public class AlertConstants
+    {
+        public const string SUCCESS = "alert alert-success alert-dismissible fade show";
+        public const string WARNING = "alert alert-warning alert-dismissible fade show";
+        public const string ERROR = "alert alert-danger alert-dismissible fade show";
+    }
+}
