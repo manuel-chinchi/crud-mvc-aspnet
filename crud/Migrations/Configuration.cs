c681bdebbd7c1ba2dd36f52d6a41e64a520554c4
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-06-18 12:59:29 -0300

    update test data for articles

--- crud/Migrations/Configuration.cs
+++ crud/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace crud.Migrations
 {
+    using crud.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +15,32 @@ namespace crud.Migrations
 
         protected override void Seed(crud.Models.ApplicationContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Articles.AddOrUpdate(
+                new Article() { Id = 1, Name = "camisa deportiva", Description = "Adidas, poliester", Quantity = 90 },
+                new Article() { Id = 2, Name = "pantalon deportivo", Description = "Adidas, poliester 95%", Quantity = 120 },
+                new Article() { Id = 3, Name = "calzones boxer", Description = "Calvin Klein", Quantity = 100 },
+                new Article() { Id = 4, Name = "calzones boxer", Description = "Kevingston", Quantity = 50 },
+                new Article() { Id = 5, Name = "jean", Description = "Nike", Quantity = 150 },
+                new Article() { Id = 6, Name = "jean", Description = "Gucci", Quantity = 180 },
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                new Article() { Id = 7, Name = "martillo neumatico", Description = "Bosch", Quantity = 120 },
+                new Article() { Id = 8, Name = "desmalezadora", Description = "Makita, año 2010", Quantity = 120 },
+                new Article() { Id = 9, Name = "amoladora", Description = "Stanley, año 2015", Quantity = 30 },
+                new Article() { Id = 10, Name = "desmalezadora", Description = "Hyundai", Quantity = 60 },
+
+                new Article() { Id = 11, Name = "heladera", Description = "Gaffa", Quantity = 60 },
+                new Article() { Id = 12, Name = "lavarropas", Description = "Electroluz", Quantity = 40 },
+                new Article() { Id = 13, Name = "cocina", Description = "General Electric", Quantity = 50 },
+                new Article() { Id = 14, Name = "heladera", Description = "LG, año 2012", Quantity = 30 },
+                new Article() { Id = 15, Name = "heladera", Description = "LG, reacondicionadas/nuevas", Quantity = 100 },
+                new Article() { Id = 16, Name = "lavavajillas", Description = "Sanyo, año 2005", Quantity = 20 },
+
+                new Article() { Id = 17, Name = "consola xbox", Description = "Microsoft, nuevas", Quantity = 200 },
+                new Article() { Id = 18, Name = "sega", Description = "modelo Genesis 3, a reparar/nuevas", Quantity = 80 },
+                new Article() { Id = 19, Name = "consola playstation 2", Description = "original, a reparar/nuevas", Quantity = 120 },
+                new Article() { Id = 20, Name = "lapiz 3D impresora", Description = "generico", Quantity = 100 },
+                new Article() { Id = 21, Name = "monopoli", Description = "Hasbro, año 2000", Quantity = 30 }
+            );
         }
     }
 }
