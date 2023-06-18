ced5447d5b26c2711316c177600d850912625e84
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-06-18 12:46:42 -0300

    add Entity Framework package and configuration connection in Web.config for database

---
+++ crud/Models/ApplicationContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace crud.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        public ApplicationContext() : base(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            #region Configure_Articles
+            modelBuilder.Entity<Article>().ToTable("articles");
+            modelBuilder.Entity<Article>().HasKey(a => a.Id);
+            modelBuilder.Entity<Article>().Property(a => a.Name).HasMaxLength(50).IsRequired();
+            modelBuilder.Entity<Article>().Property(a => a.Description).HasMaxLength(50);
+            #endregion
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
