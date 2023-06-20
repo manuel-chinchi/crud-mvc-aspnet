e611f8ed8598e8cd9cc30cc9fd7e265afb773b4a
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-06-20 14:59:30 -0300

    add migrations

---
+++ crud/Migrations/202306201607500_initial_migration.cs
@@ -0,0 +1,30 @@
+﻿namespace crud.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial_migration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Articles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Description = c.String(maxLength: 50),
+                        Quantity = c.Int(nullable: false),
+                        DateCreated = c.DateTime(nullable: false),
+                        DateUpdated = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Articles");
+        }
+    }
+}
