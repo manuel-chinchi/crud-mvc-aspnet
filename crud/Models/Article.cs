870f05baa24020a4f90c23eabaa251a4ebeb3087
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-06-20 02:01:57 -0300

    add validation for article model

--- crud/Models/Article.cs
+++ crud/Models/Article.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,10 +11,15 @@ namespace crud.Models
     {
         public int Id { get; set; }
         [DisplayName("Nombre")]
+        [RegularExpression(@"^\w+( +\w+)*$", ErrorMessage = "'Nombre' no puede ser solamente espacios en blanco, ni contener espacios al inicio o al final.")]
+        [Required(ErrorMessage = "'Nombre' no puede ser vacío.")]
+        [MaxLength(50)]
         public string Name { get; set; }
         [DisplayName("Descripción")]
         public string Description { get; set; }
         [DisplayName("Cantidad")]
+        [Required(ErrorMessage = "'Cantidad' es requerido.")]
+        [Range(1, int.MaxValue, ErrorMessage = "'Cantidad' debe ser mayor o igual a 1.")]
         public int Quantity { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime? DateUpdated { get; set; }
