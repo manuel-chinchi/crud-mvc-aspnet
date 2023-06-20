9390df709ad001086f44b80c52384b90e5a779ff
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-06-20 15:28:05 -0300

    add migration

---
+++ crud/Migrations/202306201821007_add_categories_migration.cs
@@ -0,0 +1,33 @@
+﻿namespace crud.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_categories_migration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        DateCreated = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Articles", "CategoryId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Articles", "CategoryId");
+            AddForeignKey("dbo.Articles", "CategoryId", "dbo.Categories", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Articles", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Articles", new[] { "CategoryId" });
+            DropColumn("dbo.Articles", "CategoryId");
+            DropTable("dbo.Categories");
+        }
+    }
+}
