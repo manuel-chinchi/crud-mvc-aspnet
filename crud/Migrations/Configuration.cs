149122e44acda9512aa57f045c122cd0cbc51f0a
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-06-20 15:21:54 -0300

    add category model on database

--- crud/Migrations/Configuration.cs
+++ crud/Migrations/Configuration.cs
@@ -15,31 +15,39 @@ namespace crud.Migrations
 
         protected override void Seed(crud.Models.ApplicationContext context)
         {
+            context.Categories.AddOrUpdate(
+                new Category() { Id = 1, Name = "Indumentaria" },
+                new Category() { Id = 2, Name = "Herramientas" },
+                new Category() { Id = 3, Name = "Electrodomésticos" },
+                new Category() { Id = 4, Name = "Ocio" },
+                new Category() { Id = 5, Name = "Otro" }
+            );
+
             context.Articles.AddOrUpdate(
-                new Article() { Id = 1, Name = "camisa deportiva", Description = "Adidas, poliester", Quantity = 90 },
-                new Article() { Id = 2, Name = "pantalon deportivo", Description = "Adidas, poliester 95%", Quantity = 120 },
-                new Article() { Id = 3, Name = "calzones boxer", Description = "Calvin Klein", Quantity = 100 },
-                new Article() { Id = 4, Name = "calzones boxer", Description = "Kevingston", Quantity = 50 },
-                new Article() { Id = 5, Name = "jean", Description = "Nike", Quantity = 150 },
-                new Article() { Id = 6, Name = "jean", Description = "Gucci", Quantity = 180 },
+                new Article() { Id = 1, Name = "camisa deportiva", Description = "Adidas, poliester", Quantity = 90, CategoryId = 1 },
+                new Article() { Id = 2, Name = "pantalon deportivo", Description = "Adidas, poliester 95%", Quantity = 120, CategoryId = 1 },
+                new Article() { Id = 3, Name = "calzones boxer", Description = "Calvin Klein", Quantity = 100, CategoryId = 1 },
+                new Article() { Id = 4, Name = "calzones boxer", Description = "Kevingston", Quantity = 50, CategoryId = 1 },
+                new Article() { Id = 5, Name = "jean", Description = "Nike", Quantity = 150, CategoryId = 1 },
+                new Article() { Id = 6, Name = "jean", Description = "Gucci", Quantity = 180, CategoryId = 1 },
 
-                new Article() { Id = 7, Name = "martillo neumatico", Description = "Bosch", Quantity = 120 },
-                new Article() { Id = 8, Name = "desmalezadora", Description = "Makita, año 2010", Quantity = 120 },
-                new Article() { Id = 9, Name = "amoladora", Description = "Stanley, año 2015", Quantity = 30 },
-                new Article() { Id = 10, Name = "desmalezadora", Description = "Hyundai", Quantity = 60 },
+                new Article() { Id = 7, Name = "martillo neumatico", Description = "Bosch", Quantity = 120, CategoryId = 2 },
+                new Article() { Id = 8, Name = "desmalezadora", Description = "Makita, año 2010", Quantity = 120, CategoryId = 2 },
+                new Article() { Id = 9, Name = "amoladora", Description = "Stanley, año 2015", Quantity = 30, CategoryId = 2 },
+                new Article() { Id = 10, Name = "desmalezadora", Description = "Hyundai", Quantity = 60, CategoryId = 2 },
 
-                new Article() { Id = 11, Name = "heladera", Description = "Gaffa", Quantity = 60 },
-                new Article() { Id = 12, Name = "lavarropas", Description = "Electroluz", Quantity = 40 },
-                new Article() { Id = 13, Name = "cocina", Description = "General Electric", Quantity = 50 },
-                new Article() { Id = 14, Name = "heladera", Description = "LG, año 2012", Quantity = 30 },
-                new Article() { Id = 15, Name = "heladera", Description = "LG, reacondicionadas/nuevas", Quantity = 100 },
-                new Article() { Id = 16, Name = "lavavajillas", Description = "Sanyo, año 2005", Quantity = 20 },
+                new Article() { Id = 11, Name = "heladera", Description = "Gaffa", Quantity = 60, CategoryId = 3 },
+                new Article() { Id = 12, Name = "lavarropas", Description = "Electroluz", Quantity = 40, CategoryId = 3 },
+                new Article() { Id = 13, Name = "cocina", Description = "General Electric", Quantity = 50, CategoryId = 3 },
+                new Article() { Id = 14, Name = "heladera", Description = "LG, año 2012", Quantity = 30, CategoryId = 3 },
+                new Article() { Id = 15, Name = "heladera", Description = "LG, reacondicionadas/nuevas", Quantity = 100, CategoryId = 3 },
+                new Article() { Id = 16, Name = "lavavajillas", Description = "Sanyo, año 2005", Quantity = 20, CategoryId = 3 },
 
-                new Article() { Id = 17, Name = "consola xbox", Description = "Microsoft, nuevas", Quantity = 200 },
-                new Article() { Id = 18, Name = "sega", Description = "modelo Genesis 3, a reparar/nuevas", Quantity = 80 },
-                new Article() { Id = 19, Name = "consola playstation 2", Description = "original, a reparar/nuevas", Quantity = 120 },
-                new Article() { Id = 20, Name = "lapiz 3D impresora", Description = "generico", Quantity = 100 },
-                new Article() { Id = 21, Name = "monopoli", Description = "Hasbro, año 2000", Quantity = 30 }
+                new Article() { Id = 17, Name = "consola xbox", Description = "Microsoft, nuevas", Quantity = 200, CategoryId = 4 },
+                new Article() { Id = 18, Name = "sega", Description = "modelo Genesis 3, a reparar/nuevas", Quantity = 80, CategoryId = 4 },
+                new Article() { Id = 19, Name = "consola playstation 2", Description = "original, a reparar/nuevas", Quantity = 120, CategoryId = 4 },
+                new Article() { Id = 20, Name = "lapiz 3D impresora", Description = "generico", Quantity = 100, CategoryId = 4 },
+                new Article() { Id = 21, Name = "monopoli", Description = "Hasbro, año 2000", Quantity = 30, CategoryId = 4 }
             );
         }
     }
--- crud/Models/ApplicationContext.cs
+++ crud/Models/ApplicationContext.cs
@@ -10,6 +10,7 @@ namespace crud.Models
     public class ApplicationContext : DbContext
     {
         public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         public ApplicationContext() : base(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString)
         {
@@ -22,6 +23,12 @@ namespace crud.Models
             modelBuilder.Entity<Article>().HasKey(a => a.Id);
             modelBuilder.Entity<Article>().Property(a => a.Name).HasMaxLength(50).IsRequired();
             modelBuilder.Entity<Article>().Property(a => a.Description).HasMaxLength(50);
+            #endregion
+
+            #region Configure_Categories
+            modelBuilder.Entity<Category>().ToTable("Categories");
+            modelBuilder.Entity<Category>().HasKey(c => c.Id);
+            modelBuilder.Entity<Category>().Property(c => c.Name).HasMaxLength(50).IsRequired();
             #endregion
 
             base.OnModelCreating(modelBuilder);
