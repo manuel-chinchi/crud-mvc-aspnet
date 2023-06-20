865982a5caa6fa81ae9499161f8cb81bc9e48d2a
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-06-20 15:08:03 -0300

    add category model

---
+++ crud/Models/CategoryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crud.Models
+{
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public virtual ICollection<Article> Articles { get; set; }
+
+        public CategoryModel() { DateCreated = DateTime.Now; }
+    }
+}
