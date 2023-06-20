c96f044589ec11fac3a5eec8e23548f27571c6f6
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-06-20 00:13:39 -0300

    custom labels on tables

--- crud/Scripts/datatables-custom.js
+++ crud/Scripts/datatables-custom.js
@@ -2,5 +2,18 @@
 // https://datatables.net/reference/option/language
 
 $(document).ready(function () {
-    $('table').DataTable({});
+    $('table').DataTable({
+        language: {
+            info: 'Mostrando registro _START_ hasta el _END_',
+            infoEmpty: '',
+            infoFiltered: '',
+            zeroRecords: 'No se encontraron registros coincidentes',
+            search: 'Buscar:',
+            lengthMenu: 'Mostrar _MENU_ registros',
+            paginate: {
+                previous: 'Anterior',
+                next: 'Siguiente'
+            }
+        },
+    });
 });
