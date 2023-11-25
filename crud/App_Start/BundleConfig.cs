90ed422ae3c380160554edc2a24abffae2cec139
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2023-11-25 14:10:04 -0300

    add button for export table content
    add functionality to export table data to CSV, Excel, PDF formats

--- crud/App_Start/BundleConfig.cs
+++ crud/App_Start/BundleConfig.cs
@@ -32,9 +32,35 @@ namespace crud
                     "~/Content/jquery.dataTables.min.css"
                 ));
 
+            // El archivo buttons.dataTables.min.css no viene en el paquete Nugget "datatables buttons"
+            // se instala aparte desde https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css
+            bundles.Add(new StyleBundle("~/Content/datatables-buttons").Include(
+                    "~/Content/buttons.dataTables.min.css",
+                    "~/Content/datatables-buttons-custom.css"
+                ));
+
             bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
                     "~/Scripts/datatables-custom.js",
                     "~/Scripts/jquery.dataTables.min.js"
+                ));
+
+            // Los archivos jszip.min.jq, pdfmake.min.js y vfs_fonts.js no vienen en el paquete Nugget "datatables buttons"
+            // se descargan aparte desde 
+            // https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js
+            // https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js
+            // https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js
+            bundles.Add(new ScriptBundle("~/bundles/datatables-buttons").Include(
+                    "~/Scripts/dataTables.buttons.js",
+                    // button excel
+                    "~/Scripts/jszip.min.js",
+                    // button pdf
+                    "~/Scripts/pdfmake.min.js",
+                    "~/Scripts/vfs_fonts.js",
+                    // other buttons
+                    //"~/Scripts/buttons.flash.min.js",
+                    //"~/Scripts/buttons.colVis.min.js",
+                    "~/Scripts/buttons.html5.min.js",
+                    "~/Scripts/buttons.print.min.js"
                 ));
         }
     }
--- crud/Scripts/datatables-custom.js
+++ crud/Scripts/datatables-custom.js
@@ -1,5 +1,6 @@
 ﻿// Documentation 
 // https://datatables.net/reference/option/language
+// https://datatables.net/extensions/buttons/examples/html5/columns.html
 
 $(document).ready(function () {
     $('table').DataTable({
@@ -13,7 +14,47 @@ $(document).ready(function () {
             paginate: {
                 previous: 'Anterior',
                 next: 'Siguiente'
+            },
+            buttons: {
+                copyTitle: 'Copiado al portapapeles',
+                copySuccess: {
+                    _: '%d líneas copiadas',
+                },
             }
         },
+        dom: 'lBfrtip',
+        buttons: [
+            {
+                extend: 'collection',
+                text: 'Exportar',
+                buttons: [
+                    {
+                        extend: 'copy',
+                        text: 'Copiar',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4]
+                        }
+                    },
+                    {
+                        extend: 'csv',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4]
+                        }
+                    },
+                    {
+                        extend: 'excel',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4]
+                        }
+                    },
+                    {
+                        extend: 'pdf',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4]
+                        },
+                    },
+                ],
+            },
+        ]
     });
 });
